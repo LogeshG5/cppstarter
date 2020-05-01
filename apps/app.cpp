3c01472216b7e3b7f39dc6b3259a635053691954
Logesh <logeshg5@gmail.com>
2020-05-01 16:26:43 +0530

    Add .clang-format

--- apps/app.cpp
+++ apps/app.cpp
@@ -6,12 +6,13 @@
 #include <tuple>
 #include <vector>
 
-int main() {
-  std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
+int main()
+{
+   std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
 
-  auto [mean, moment] = accumulate_vector(input);
+   auto [mean, moment] = accumulate_vector(input);
 
-  fmt::print("Mean: {}, Moment: {}\n", mean, moment);
+   fmt::print("Mean: {}, Moment: {}\n", mean, moment);
 
-  return 0;
+   return 0;
 }
--- lib/lib.cpp
+++ lib/lib.cpp
@@ -11,12 +11,11 @@
 
 namespace ba = boost::accumulators;
 
-std::tuple<double, double>
-accumulate_vector(const std::vector<double> &values) {
+std::tuple<double, double> accumulate_vector(const std::vector<double> &values)
+{
+   ba::accumulator_set<double, ba::stats<ba::tag::mean, ba::tag::moment<2>>> acc;
 
-  ba::accumulator_set<double, ba::stats<ba::tag::mean, ba::tag::moment<2>>> acc;
+   std::for_each(std::begin(values), std::end(values), std::ref(acc));
 
-  std::for_each(std::begin(values), std::end(values), std::ref(acc));
-
-  return {ba::mean(acc), ba::moment<2>(acc)};
+   return {ba::mean(acc), ba::moment<2>(acc)};
 }
